a55225d6a395ee11374d78545eb4631e0ea6c1c9
JeysonDevv <jeysongabriel.jimenez@gmail.com>
2023-04-11 13:17:22 -0600

    Ejercicio 2

---
+++ longitudCadena.js
@@ -0,0 +1,14 @@
+//Esta es la definición de la función obtenerLongitud
+//Toma un parámetro 'cadena', que debe ser una cadena de texto
+//Devuelve la longitud de la cadena 
+
+function obtenerLongitud(cadena){
+    return cadena.length;
+}
+
+//Ejemplos de uso de la función
+//Llamamos a la función con distintos argumentos para obtener la longitud de las cadenas 
+
+console.log(obtenerLongitud("Hola mundo")); //devuelve 10
+console.log(obtenerLongitud("Ingenieria en sistemas")); //devuelve 22
+console.log(obtenerLongitud("")); //devuelve 0
